4f8667ae3f25ffb92f3f096d3acf29ee40adf7b4
Einar Alex <einaralex@Einars-MacBook-Pro.local>
2019-01-15 00:02:20 +0000

    Added Babel and started intialized interaction with contract locally

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,51 @@
-var PokerTournamentContract = require("../../build/contracts/PokerTournament.json")
-var Web3 = require('web3');
-let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
 
-const networkId = web3.eth.net.getId();
-const deployedNetwork = PokerTournamentContract.networks[networkId];
-const instance = new web3.eth.Contract(
-    PokerTournamentContract.abi,
-    deployedNetwork && deployedNetwork.address
-)
+import PokerTournament from "../../build/contracts/PokerTournament.json";
+import Web3 from 'web3';
 
-console.log(instance)
+var web3 = new Web3(Web3.givenProvider || "http://localhost:9545");
+//var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
+var eth = web3.eth
+
+// creates a contract object for solidity contracts
+var PokerTournamentContract = new eth.Contract(PokerTournament.abi)
+
+// coinbase address - the recipient of the mining reward, defaults to the first account
+var coinbase = eth.getCoinbase();
+// var balance = eth.getBalance(coinbase);
+
+
+/* eth.call({
+    to: "address"
+    data: "data"
+}: object,
+ [defaultBlock:number|string] 
+ [callback:func]
+) */
+/* web3.eth.estimateGas(callContract object) */
+
+async function getAccounts () {
+    return await eth.getAccounts();
+}
+
+async function getDefaultAccount() {
+    let accounts = await getAccounts();
+    return accounts[1];
+}
+
+async function getAccountBalance(account) {
+    return await eth.getBalance(account);
+}
+
+async function getInfo() {
+    const defaultAccount = await getDefaultAccount();
+    console.log('Accounts', await getAccounts())
+    console.log('DefaultAccount', defaultAccount)
+    console.log('Coinbase', await eth.getCoinbase())
+    console.log('DefaultAccountBalance', await getAccountBalance(defaultAccount))
+    //console.log('Coinbase balance', await eth.getBalance(coinbase)
+}
+
+getInfo()
+//console.log('Default Account', eth.defaultAccount)
+
+// https://github.com/ethereum/wiki/wiki/JavaScript-API

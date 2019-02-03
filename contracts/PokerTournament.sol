4d8c8f24f424170392023d802a498e5c7624a208
Einar Alex <einaralex@Einars-MacBook-Pro.local>
2019-02-03 04:15:58 +0000

    Added deposit, ballot consensus.. completly untested

--- contracts/PokerTournament.sol
+++ contracts/PokerTournament.sol
@@ -2,15 +2,21 @@ pragma solidity ^0.4.25;
 
 // https://github.com/MeadowSuite/Meadow/wiki/Using-the-VSCode-Solidity-Debugger
 
+
+
 contract PokerTournament {
     uint public buyIn;
+    uint public deposit;
     uint public prizePool;
+    uint public depositPool;
+    bool public votingHasStarted;
     address[] private playersRegistered;
     address[] private playersVoted;
 
-    mapping(address => uint) private playerBalance;
+    mapping(address => bool) private isRegistered;
     mapping(address  => address[]) private ballot;
-    
+    mapping(address => uint) private count;
+
     event LogDeposit (address sender, uint amount, uint balance);
     event LogHandout (uint potiumSize, uint prizeCalculation, uint place );
     event LogHandout2 (uint prize, uint prizePool);
@@ -25,39 +31,50 @@ contract PokerTournament {
     }
     /* solhint-enable no-empty-blocks */
 
+    function test(uint number) public pure returns (uint) {
+        return number * 1 / 4;
+    }
+
     function deposit() public payable {
         emit LogDeposit(msg.sender, msg.value, msg.sender.balance); 
         address depositeeAddress = msg.sender;
         uint depositeeFunds = msg.value;
+        require(votingHasStarted == false, "Registration has ended");
+        require(isRegistered[depositeeAddress] == false, "A player can only deposit once.");
 
-        require(playerBalance[depositeeAddress] == uint(0), "A player can only deposit once.");
-
-        /* First depositee controls the buy-in amount */
+        /* First depositee sets the buy-in amount */
         if (playersRegistered.length == 0) {
-            setBuyIn(depositeeFunds);
+            setBuyInAndDeposit(depositeeFunds);
         }
         else {
-            require(depositeeFunds == uint(buyIn), "Deposit amount has to match the buy-in amount.");
+            require(depositeeFunds == uint(buyIn+deposit), "Deposit amount has to match the buy-in + deposit amount.");
         }
-        // hold a record over registered players
-        playersRegistered.push(depositeeAddress);
-        
-        // holds a record of balance tied to an address, possibly not needed
-        playerBalance[depositeeAddress] += depositeeFunds;
 
-        prizePool += depositeeFunds;
+        depositPool += deposit;
+        prizePool += buyIn;
+
+        playersRegistered.push(depositeeAddress);
+        isRegistered[depositeeAddress] = true;
     }
 
     function getPlayerCount() public view returns (int) {
         return int(playersRegistered.length);
     }
 
+    function transferDepositBack() public {
+        for(uint i = 0; i < playersVoted.length; i++ ) {
+            playersVoted[i].transfer(deposit);
+            depositPool -= deposit;
+        }
+    }
+
     // First iteration will trust that every player votes correctly:
     // player sends in a listOfWinners array arranged from first to last place
     function voteForWinner(address[] memory listOfWinners) public payable {
+        votingHasStarted = true;
         emit LogVoting(prizePool, allPlayersHaveVoted());
         address depositeeAddress = msg.sender;
-        require(playerBalance[depositeeAddress] != uint(0), "Voter should be participating.");
+        require(isRegistered[depositeeAddress] == true, "Voter should be participating.");
 
         require(ballot[depositeeAddress].length < 1, "A player can only vote once");
         require(
@@ -71,19 +88,68 @@ contract PokerTournament {
         // maintain an iterable record for who has voted
         playersVoted.push(msg.sender);
 
-        if (allPlayersHaveVoted()) {
-            // TODO: check if all votes match
-            // TODO: iterate through the potium in reverse (3-2-1) 
-            //  and use playerAccount.transfer(address(this).balance) to transfer the rest/dust.
-            //  then selfdestruct();
-            for(uint i = 0; i < getPotiumSize(); i++) {
-                handOutReward(listOfWinners[i], i);
+        if (allPlayersHaveVoted() || timedOut()) {
+
+            transferDepositBack();
+
+            if (majorityHasVoted()) {
+                // address[] memory winningBallot = getWinningBallot();
+                
+                // Færa inn í handOutRewards?
+                for(uint place = getPotiumSize()-1; place >= 0; place--) {
+                    handOutReward(getWinningBallot()[place], place);
+                }
+                // for(uint place = 0; place < getPotiumSize(); place++) {
+                //     handOutReward(listOfWinners[place], place);
+                // }
             }
-            
-            // TODO: Reset everything, not just the prizepool
+            // TODO: selfDestruct
             prizePool = 0;
         }
     }
+
+    function timedOut() public pure returns (bool) {
+        // TODO Timeout á deposit
+        return false;
+    }
+
+    function majorityHasVoted() public view returns (bool) {
+        // uint() floors integers, add one to get ceiling.
+        uint majority = uint(playersRegistered.length * 1 / 2) + 1;
+        return playersVoted.length >= majority;
+    }
+
+    function getWinningBallot() public view returns (address[]) {
+
+        uint max = 0;
+        address[] memory winningBallot;
+        address[] memory thisBallot;
+        address[] memory nextBallot;
+
+        for(uint i = 0; i < playersVoted.length; i++ ) {
+            uint counter = 0;
+            thisBallot = ballot[playersVoted[i]];
+            for(uint j = i + 1; j < playersVoted.length; ) {
+                nextBallot = ballot[playersVoted[j]];
+
+                bool isMatch;
+                // https://ethereum.stackexchange.com/questions/51846/compare-memory-pointers-in-solidity
+                assembly {
+                    isMatch := eq(thisBallot, nextBallot)
+                }
+                if (isMatch) {
+                    counter += 1;
+                }
+            }
+            if(counter > max) {
+                max = counter;
+                winningBallot = thisBallot;
+            }
+        }
+        return winningBallot;
+    }
+
+
     function calculatePrize(uint place) public view returns (uint) {
         uint potiumSize = getPotiumSize();
         uint prizeMath = getPrizeCalculation();
@@ -97,13 +163,17 @@ contract PokerTournament {
     function handOutReward(address playerAccount, uint place) public payable {
         emit LogHandout(getPotiumSize(), getPrizeCalculation(), place);
 
-        require(playerBalance[playerAccount] != uint(0), "Player has to be registered.");
+        require(isRegistered[playerAccount] == true, "Player has to be registered.");
 
         uint prize = calculatePrize(place); 
 
+        // The top player gets the "dust" + depositPool if someone didn't vote
+        if (place == 0) {
+            prize = getContractBalance() + depositPool;
+        }
+
         emit LogHandout2(prize, prizePool);
-        require(address(this).balance >= prize, "Contract does not have enough credit");
-        
+
         playerAccount.transfer(prize);
     }
 
@@ -140,9 +210,12 @@ contract PokerTournament {
         return (playersRegistered.length == playersVoted.length) ? true : false;
     }
 
-    function setBuyIn(uint amount) private {
+    function setBuyInAndDeposit(uint amount) private {
         require(amount >= 0, "The buy-in amount has to be positive.");
-        buyIn = amount;
+        uint depositAmount = amount * 1 / 4;
+
+        deposit = depositAmount;
+        buyIn = amount - depositAmount;
     }
 }
 

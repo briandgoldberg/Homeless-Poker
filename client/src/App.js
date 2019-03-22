49f37f1f1998f1ad22576a6f4e56256b8083c09b
Einar Alexander Eymundsson <einar.alexander.eymundsson@samskip.com>
2019-03-22 20:33:14 +0000

    Revert "Are the hooks working?"
    This reverts commit a5aab43b8f27333af218473169a9655db5578965.

--- client/src/App.js
+++ client/src/App.js
@@ -84,6 +84,7 @@ class App extends Component {
         this.setState({ voted: true, votedPlayers: this.state.votedPlayers +1 })
         console.log(receipt)
       })
+
     this.getLobbyInfo()
   }
   vote = async (ballot) => {

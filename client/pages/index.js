64e648529a51aa9197d161703d12e1d3cd5e324e
Einar Alexander Eymundsson <einar.alexander.eymundsson@samskip.com>
2019-04-11 00:24:11 +0000

    index site WIP

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,36 +1,45 @@
-import React from 'react'
+import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
-import MuiLink from '@material-ui/core/Link'
-import ProTip from '../src/ProTip'
-import Link from '../src/Link'
+import React, { Component } from 'react'
+import * as Link from '../src/Link'
 
-function MadeWithLove() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Built with love by the '}
-      <MuiLink color="inherit" href="https://material-ui.com/">
-        Material-UI
-      </MuiLink>
-      {' team.'}
-    </Typography>
-  )
-}
+const Input = (placeholder, onChange) => (
+  <input placeholder={placeholder} onChange={onChange()} />
+)
 
-export default function Index() {
-  return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js v4-alpha example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <MadeWithLove />
-      </Box>
-    </Container>
-  )
+const Button = (title, onClick) => (
+  <button type="submit" onClick={onClick()}>
+    {title}
+  </button>
+)
+
+export default class Index extends Component {
+  render() {
+    const { contractAddress, value } = this.state
+    if (!web3) {
+      return <div>Loading Web3, accounts, and contract...</div>
+    }
+    return (
+      <Container maxWidth="sm">
+        <Box my={4}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Next.js v4-alpha example
+          </Typography>
+          <Link href="/about" color="secondary">
+            Go to the about page
+          </Link>
+          <Button title="Start" onClick={() => this.start()} />
+          <Input placeholder="address" onChange={e => this.handleAddress(e)} />
+          <Input placeholder="0.0001" onChange={e => this.handleValue(e)} />
+          <Button
+            title="Deposit ether"
+            onClick={() =>
+              value && this.join(contractAddress, 'name', value, 'TEST')
+            }
+          />
+        </Box>
+      </Container>
+    )
+  }
 }

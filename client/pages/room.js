4ee1d68c708160d547f18e7e09edcc0235d0f68f
Einar Alexander Eymundsson <einar.alexander.eymundsson@samskip.com>
2019-05-26 13:17:01 +0000

    Added rearrangable list to the voting page

--- client/pages/room.js
+++ client/pages/room.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, List } from 'components'
+import arrayMove from 'array-move'
 
+const mockRegisteredPlayer = ['einar', 'sveinar', 'geinar', 'meinar']
 const Room = () => {
-  return <p> this is the room</p>
+  const [listOrder, setListOrder] = useState(mockRegisteredPlayer)
+  const rearrangeList = ({ oldIndex, newIndex }) => {
+    setListOrder(arrayMove(listOrder, oldIndex, newIndex))
+  }
+  return (
+    <>
+      <List items={listOrder} onChange={rearrangeList} />
+      <Button title="Vote" onClick={() => console.log('vote')} />
+    </>
+  )
 }
 export default Room

ba3756b97e02d3d513ea3e14b783ddcf90ad5e83
Einar Alexander Eymundsson <einar.alexander.eymundsson@samskip.com>
2019-05-26 20:48:10 +0000

    moved Link from button component, wasn't quite working, have to come back to this later

--- client/components/Button/index.js
+++ client/components/Button/index.js
@@ -4,21 +4,17 @@ import PropTypes from 'prop-types'
 import MuiButton from '@material-ui/core/Button'
 
 function Button(props) {
-  const { href, onClick, title } = props
+  const { onClick, title } = props
   return (
     <MuiButton variant="contained" color="primary" onClick={() => onClick()}>
-      <Link href={href}>{title}</Link>
+      {title}
     </MuiButton>
   )
 }
 
 Button.propTypes = {
-  // eslint-disable-next-line react/require-default-props
-  href: PropTypes.string,
   onClick: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired
 }
-Button.defaultProps = {
-  href: ''
-}
+
 export default Button

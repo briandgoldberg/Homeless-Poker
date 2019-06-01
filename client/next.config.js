c3c7c1447c7e89a52b11f6d01c6159465e3e6bb9
Einar Alexander Eymundsson <einar.alexander.eymundsson@samskip.com>
2019-06-01 16:29:10 +0000

    Downgraded to beta37 because of some production build vs. Metamask issue

--- client/next.config.js
+++ client/next.config.js
@@ -1,13 +1,19 @@
 const withPlugins = require('next-compose-plugins')
 const withCSS = require('@zeit/next-css')
 const withSass = require('@zeit/next-sass')
+const webpack = require('webpack')
 
 module.exports = withPlugins([[withCSS], [withSass]], {
-  target: 'serverless',
+  // target: 'serverless',
   ssr: true,
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: '[local]___[hash:base64:5]'
+  },
+  webpack(config) {
+    config.plugins.push(new webpack.IgnorePlugin(/^electron$/))
+
+    return config
   }
 })
